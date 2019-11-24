387879037ea4f45cc67a6ca1629b6b9a08fe301e
jaglees <james.lees@accenture.com>
2019-11-24 17:19:21 +0000

    Added comments

--- piBitControl.py
+++ piBitControl.py
@@ -5,6 +5,8 @@ devices={}
 filepath = './microbits.cnf'
 print ("Initialising microbits found in ", filepath)
 try: 
+
+    # Read list of Microbit MAC addresses and attempt to connect to each
     with open(filepath) as fp:
         line = fp.readline()
         while line:
@@ -21,7 +23,6 @@ try:
                                     pin_service=False,
                                     temperature_service=True)
                 print ("....Initialised")
-                #devices[key]=x
 
                 print ("....Conneting")
                 devices[key].connect()
@@ -35,22 +36,28 @@ except:
     print("Failed to open config file")
     exit()
 
-looping = True
+# Check that at least one device is connected to - if not exit
 if (devices is None or len(devices)==0):
     print ("Connection to all devices failed - exiting")
     exit()
 
+looping = True
 while looping:
+
+    # Check each device in turn
     for key in devices:
         
+        # If the A button is pressed - exit the loop (a kill switch)
         if (devices[key].button_a > 0):
             print("Device [", key, "] Button A Pressed" )
             looping=False
         
+        # Print the temperature from the device 
         print('Temperature [', key, '] = ', devices[key].temperature)
     
     time.sleep(1)
 
+# Tidy up by disconnecting from all devices
 print ("Disconnecting from all devices")
 for key in devices:
     print ("..Device [",key,"]")
